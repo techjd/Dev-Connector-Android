a9a4b1a9381c144c740482e67350db29275deae8
Jaydeep <jaydeepparmar253@gmail.com>
2022-02-11 13:51:03 +0530

    Added Implementations for Getting Single Posts, All Comments , Commenting on a Post

--- app/src/main/java/com/techjd/devconnector/respository/PostsRepository.kt
+++ app/src/main/java/com/techjd/devconnector/respository/PostsRepository.kt
@@ -1,6 +1,7 @@
 package com.techjd.devconnector.respository
 
 import com.techjd.devconnector.api.DevConnectorService
+import com.techjd.devconnector.data.models.CommentBody
 import com.techjd.devconnector.data.models.UserPosts.NewPostResponse.NewPost
 import com.techjd.devconnector.data.models.UserPosts.Posts
 import retrofit2.Response
@@ -12,4 +13,10 @@ object PostsRepository {
 
     suspend fun newPost(token: String, newPost: NewPost) =
         DevConnectorService.devConnectorInstance.updatePosts(token, newPost)
+
+    suspend fun getSinglePost(token: String, postId: String) =
+        DevConnectorService.devConnectorInstance.getSinglePost(token, postId)
+
+    suspend fun getAllComments(token: String, id: String, commentBody: CommentBody) =
+        DevConnectorService.devConnectorInstance.postAComment(token, id, commentBody)
 }
--- app/src/main/java/com/techjd/devconnector/viewmodels/PostsViewModel.kt
+++ app/src/main/java/com/techjd/devconnector/viewmodels/PostsViewModel.kt
@@ -6,9 +6,12 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.techjd.devconnector.Utils.Resource
+import com.techjd.devconnector.data.models.CommentBody
+import com.techjd.devconnector.data.models.CommentsResponse.UpdatesComments
 import com.techjd.devconnector.data.models.UserPosts.NewPostResponse.NewPost
 import com.techjd.devconnector.data.models.UserPosts.NewPostResponse.PostResponse
 import com.techjd.devconnector.data.models.UserPosts.Posts
+import com.techjd.devconnector.data.models.UserPosts.SinglePost.SinglePost
 import com.techjd.devconnector.respository.PostsRepository
 import kotlinx.coroutines.launch
 
@@ -17,6 +20,10 @@ class PostsViewModel : ViewModel() {
     val postLiveData: LiveData<Resource<Posts>> = _posts
     private val _postsResponse: MutableLiveData<Resource<PostResponse>> = MutableLiveData()
     val postsResponseLiveData: LiveData<Resource<PostResponse>> = _postsResponse
+    private val _singlePost: MutableLiveData<Resource<SinglePost>> = MutableLiveData()
+    val singlePostLiveData: LiveData<Resource<SinglePost>> = _singlePost
+    private val _comment: MutableLiveData<Resource<UpdatesComments>> = MutableLiveData()
+    val comment: LiveData<Resource<UpdatesComments>> = _comment
 
     suspend fun getPosts(token: String) {
         Log.d("PostsViewModel", "getPosts: Called")
@@ -40,6 +47,30 @@ class PostsViewModel : ViewModel() {
                 _postsResponse.value = Resource.Success(data = response.body()!!)
             } else {
                 _postsResponse.value = Resource.Error(message = "Some Error Occurred")
+            }
+        }
+    }
+
+    suspend fun getSinglePost(token: String, postId: String) {
+        viewModelScope.launch {
+            _singlePost.value = Resource.Loading(message = "Post is Uploading")
+            val response = PostsRepository.getSinglePost(token, postId)
+            if (response.isSuccessful) {
+                _singlePost.value = Resource.Success(data = response.body()!!)
+            } else {
+                _singlePost.value = Resource.Error(message = "Some Error Occurred")
+            }
+        }
+    }
+
+    suspend fun postAComment(token: String, id: String, commentBody: CommentBody) {
+        viewModelScope.launch {
+            _comment.value = Resource.Loading(message = "Comment is Loading")
+            val response = PostsRepository.getAllComments(token, id, commentBody)
+            if (response.isSuccessful) {
+                _comment.value = Resource.Success(data = response.body()!!)
+            } else {
+                _comment.value = Resource.Error(message = "Some Error Occurred")
             }
         }
     }
