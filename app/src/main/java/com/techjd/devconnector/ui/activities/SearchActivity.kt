73835b8269f693110ed6f406e42623db2970ed62
Jaydeep <jaydeepparmar253@gmail.com>
2022-02-13 14:21:53 +0530

    Added Search Functionality

--- app/src/main/java/com/techjd/devconnector/ui/activities/SearchActivity.kt
+++ app/src/main/java/com/techjd/devconnector/ui/activities/SearchActivity.kt
@@ -1,21 +1,105 @@
 package com.techjd.devconnector.ui.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.appbar.MaterialToolbar
 import com.techjd.devconnector.R
+import com.techjd.devconnector.Utils.Status
+import com.techjd.devconnector.data.models.profiles.ProfileItem
+import com.techjd.devconnector.ui.fragments.ProfilesAdapter
+import com.techjd.devconnector.viewmodels.ProfileViewModel
+import kotlinx.coroutines.launch
+import java.util.*
+import kotlin.collections.ArrayList
 
 class SearchActivity : AppCompatActivity() {
     lateinit var topSearchBar: MaterialToolbar
+    lateinit var edtSearch: EditText
+    lateinit var profilesRv: RecyclerView
+    lateinit var progressBar: ProgressBar
+    lateinit var profileList: ArrayList<ProfileItem>
+    lateinit var profilesAdapter: ProfilesAdapter
+    val profileViewModel: ProfileViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
         topSearchBar = findViewById(R.id.topSearchBar)
+        edtSearch = findViewById(R.id.edtSearch)
+        profilesRv = findViewById(R.id.searchResults)
+        progressBar = findViewById(R.id.searchProgressBar)
+
+        profilesRv.layoutManager = LinearLayoutManager(this)
+
         topSearchBar.setNavigationOnClickListener {
             onBackPressed()
         }
 
+        lifecycleScope.launch {
+            profileViewModel.getAllProfile()
+        }
+
+        profileViewModel.allProfile.observe(this) { profiles ->
+            when (profiles.status) {
+                Status.SUCCESS -> {
+                    profilesAdapter = ProfilesAdapter(
+                        profiles.data!!,
+                        onClickListener = { view, s ->
+                            Toast.makeText(this, s, Toast.LENGTH_SHORT).show()
+                        }
+                    )
+                    profilesRv.adapter = profilesAdapter
+                    profileList = profiles.data!!
+                    edtSearch.visibility = View.VISIBLE
+                    progressBar.visibility = View.GONE
+
+                }
+                Status.LOADING -> {
+                    progressBar.visibility = View.VISIBLE
+                }
+                Status.ERROR -> {
+                    progressBar.visibility = View.GONE
+                }
+            }
+        }
+
+        edtSearch.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                filter(s.toString())
+            }
+
+        })
+
+    }
+
+    private fun filter(text: String) {
+        val filteredList: ArrayList<ProfileItem> = ArrayList()
+        for (item in profileList) {
+            if (item.user.name.lowercase(Locale.getDefault())
+                    .contains(text.lowercase(Locale.getDefault()))
+            ) {
+                filteredList.add(item)
+            }
+        }
+        profilesAdapter.filterList(filteredList)
     }
 
     override fun onBackPressed() {
--- app/src/main/java/com/techjd/devconnector/viewmodels/ProfileViewModel.kt
+++ app/src/main/java/com/techjd/devconnector/viewmodels/ProfileViewModel.kt
@@ -1,28 +1,63 @@
 package com.techjd.devconnector.viewmodels
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.techjd.devconnector.Utils.Resource
 import com.techjd.devconnector.data.models.UserInfo.UserInfo
+import com.techjd.devconnector.data.models.profiles.Profile
+import com.techjd.devconnector.data.models.profiles.ProfileBody
+import com.techjd.devconnector.data.models.profiles.ProfileItem
+import com.techjd.devconnector.data.models.profiles.profileResponse.CreatedProfileResponse
 import com.techjd.devconnector.respository.PostsRepository
+import com.techjd.devconnector.respository.ProfileRepository
 import com.techjd.devconnector.respository.UserInfoRepository
 import kotlinx.coroutines.launch
 import retrofit2.Response
 
 class ProfileViewModel : ViewModel() {
     private val _userInfo: MutableLiveData<Resource<UserInfo>> = MutableLiveData()
-    var userInfo: LiveData<Resource<UserInfo>> = _userInfo
+    val userInfo: LiveData<Resource<UserInfo>> = _userInfo
+    private val _allProfile: MutableLiveData<Resource<Profile>> = MutableLiveData()
+    val allProfile: LiveData<Resource<Profile>> = _allProfile
+    private val _profileResponse: MutableLiveData<Resource<CreatedProfileResponse>> = MutableLiveData()
+    val profileResponse: LiveData<Resource<CreatedProfileResponse>> = _profileResponse
 
     suspend fun getUserInfo(token: String) {
         viewModelScope.launch {
             _userInfo.value = Resource.Loading("Data is Loading")
             val response = UserInfoRepository.getUserInfo(token)
+            Log.d("CODE ", "getUserInfo: ${response.code()}")
             if (response.isSuccessful) {
                 _userInfo.value = Resource.Success(response.body()!!)
             } else {
                 _userInfo.value = Resource.Error("Some Error Occurred")
+            }
+        }
+    }
+
+    suspend fun getAllProfile() {
+        viewModelScope.launch {
+            _allProfile.value = Resource.Loading("Data is Loading")
+            val response = ProfileRepository.getAllProfiles()
+            if (response.isSuccessful) {
+                _allProfile.value = Resource.Success(response.body()!!)
+            } else {
+                _allProfile.value = Resource.Error("Some Error Occurred")
+            }
+        }
+    }
+
+    suspend fun createProfile(token: String, profileBody: ProfileBody) {
+        viewModelScope.launch {
+            _profileResponse.value = Resource.Loading("Profile is Being Created")
+            val response = ProfileRepository.createProfile(token, profileBody)
+            if (response.isSuccessful) {
+                _profileResponse.value = Resource.Success(response.body()!!)
+            } else {
+                _profileResponse.value = Resource.Error("Some Error Occurred")
             }
         }
     }
