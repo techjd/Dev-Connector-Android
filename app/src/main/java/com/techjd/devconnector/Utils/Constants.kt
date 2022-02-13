fd93716a9cc291001beccd9e392491d00d7bf488
Jaydeep <jaydeepparmar253@gmail.com>
2022-02-13 14:22:20 +0530

    Replaced Deployed URL

---
+++ app/src/main/java/com/techjd/devconnector/Utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.techjd.devconnector.Utils
+
+object Constants {
+    const val SERVER_URL = "https://dev-connector-backend-server.herokuapp.com/api/"
+    const val CHAT_SERVER_URL = "https://dev-connector-backend-server.herokuapp.com/"
+}
